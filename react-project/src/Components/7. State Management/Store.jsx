6f03a33aeb8a676bc07cae07d712d2422e413415
BharteshGirmal <bharteshgirmal@gmail.com>
2025-09-19 22:44:42 +0530

    React JS - Bhartesh

--- react-project/src/Components/7. State Management/Store.jsx
+++ react-project/src/Components/7. State Management/Store.jsx
@@ -3,6 +3,7 @@ import thunkReducer from "./ThunkReducer";
 
 const store = configureStore({
   reducer: thunkReducer,
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
